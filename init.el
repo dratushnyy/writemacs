ff99e1d5c50c8e4e9b0808a2aced31d302923a05
Dmitry Ratushnyy <d.ratushnyy@gmail.com>
2026-04-16 12:16:50 +0200

    mend

--- init.el
+++ init.el
@@ -172,12 +172,15 @@
 
 
 ;; Evil
+
+
 (use-package evil
   :init
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
   (setq evil-want-C-u-scroll t)
   (setq evil-want-C-i-jump nil)
+
   :config
   (evil-mode 1)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
@@ -190,9 +193,11 @@
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
 
-  
 
-
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
 
 
 
